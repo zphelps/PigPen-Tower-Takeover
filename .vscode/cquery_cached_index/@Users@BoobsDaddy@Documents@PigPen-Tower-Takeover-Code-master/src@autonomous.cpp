052fad000c1aa03f73f855005da88cde7aa2202a
Zach Phelps <zachstarphelps@gmail.com>
2019-05-28 12:39:18 -0400

    fixing David's fuck up

--- .vscode/cquery_cached_index/@Users@BoobsDaddy@Documents@PigPen-Tower-Takeover-Code-master/src@autonomous.cpp
+++ .vscode/cquery_cached_index/@Users@BoobsDaddy@Documents@PigPen-Tower-Takeover-Code-master/src@autonomous.cpp
@@ -14,11 +14,6 @@
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-using namespace okapi;
-const int DRIVE_LEFTBACK_MOTOR_PORT = 4;
-const int DRIVE_RIGHTBACK_MOTOR_PORT = 2;
-
-auto chasis = ChassisControllerFactory::create(DRIVE_LEFTBACK_MOTOR_PORT,DRIVE_RIGHTBACK_MOTOR_PORT);
 
 void autonomous() {
 
